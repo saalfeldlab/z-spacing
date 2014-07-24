897d8f9e904e6c119c0faec091ed590ebde7630a
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2014-07-24 18:31:31 -0400

    removed src/main/java/org/janelia/waves/thickness/functions and src/test/java/org/janelia/waves/thickness/functions

--- src/main/java/org/janelia/waves/thickness/functions/DifferentiableParameterizedFunctionFactory.java
+++
@@ -1,7 +0,0 @@
-package org.janelia.waves.thickness.functions;
-
-public interface DifferentiableParameterizedFunctionFactory {
-	
-	public DifferentiableParameterizedFunction create( double[] parameters );
-	
-}
