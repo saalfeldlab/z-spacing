76d619fd3080a166b22996a70cecd3a1fe0e32db
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2014-07-24 17:21:19 -0400

    finalized variables and organized imports

--- src/main/java/org/janelia/models/FunctionFitModel.java
+++ src/main/java/org/janelia/models/FunctionFitModel.java
@@ -2,15 +2,15 @@ package org.janelia.models;
 
 import java.util.Collection;
 
-import org.apache.commons.math.FunctionEvaluationException;
-import org.apache.commons.math.optimization.OptimizationException;
-import org.apache.commons.math.optimization.fitting.CurveFitter;
-import org.apache.commons.math.optimization.fitting.ParametricRealFunction;
-
 import mpicbg.models.AbstractModel;
 import mpicbg.models.IllDefinedDataPointsException;
 import mpicbg.models.NotEnoughDataPointsException;
 import mpicbg.models.PointMatch;
+
+import org.apache.commons.math.FunctionEvaluationException;
+import org.apache.commons.math.optimization.OptimizationException;
+import org.apache.commons.math.optimization.fitting.CurveFitter;
+import org.apache.commons.math.optimization.fitting.ParametricRealFunction;
 
 @SuppressWarnings("deprecation")
 public class FunctionFitModel extends AbstractModel<FunctionFitModel> {
@@ -36,7 +36,7 @@ public class FunctionFitModel extends AbstractModel<FunctionFitModel> {
 	/**
 	 * @param parameters the parameters to set
 	 */
-	public void setParameters(double[] parameters) {
+	public void setParameters(final double[] parameters) {
 		this.parameters = parameters;
 	}
 
@@ -61,19 +61,21 @@ public class FunctionFitModel extends AbstractModel<FunctionFitModel> {
 		return fitter;
 	}
 
-	public FunctionFitModel(double[] parameters, ParametricRealFunction func,
-			CurveFitter fitter) {
+	public FunctionFitModel(final double[] parameters, final ParametricRealFunction func,
+			final CurveFitter fitter) {
 		super();
 		this.parameters = parameters;
 		this.func = func;
 		this.fitter = fitter;
 	}
 
+	@Override
 	public int getMinNumMatches() {
 		return this.parameters.length;
 	}
 
-	public <P extends PointMatch> void fit(Collection<P> matches)
+	@Override
+	public <P extends PointMatch> void fit(final Collection<P> matches)
 			throws NotEnoughDataPointsException, IllDefinedDataPointsException {
 		if ( matches.size() < getMinNumMatches() ) {
 			throw new NotEnoughDataPointsException( String.format( "Got %d, expected %d", matches.size(), getMinNumMatches() ) );
@@ -81,21 +83,21 @@ public class FunctionFitModel extends AbstractModel<FunctionFitModel> {
 		
 		this.fitter.clearObservations();
 		
-		for ( P m : matches ) {
+		for ( final P m : matches ) {
 			this.fitter.addObservedPoint( m.getWeight(), m.getP1().getW()[0], m.getP2().getW()[0] );
 		}
 		
 		try {
 			this.parameters = this.fitter.fit( this.func, this.parameters );
-		} catch (OptimizationException e) {
+		} catch (final OptimizationException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 			throw new NotEnoughDataPointsException( e.getMessage() );
-		} catch (FunctionEvaluationException e) {
+		} catch (final FunctionEvaluationException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 			throw new IllDefinedDataPointsException( e.getMessage() );
-		} catch (IllegalArgumentException e) {
+		} catch (final IllegalArgumentException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 			throw new IllDefinedDataPointsException( e.getMessage() );
@@ -105,12 +107,13 @@ public class FunctionFitModel extends AbstractModel<FunctionFitModel> {
 
 
 
-	public float[] apply(float[] location) {
-		float[] result = new float[ location.length ];
+	@Override
+	public float[] apply(final float[] location) {
+		final float[] result = new float[ location.length ];
 		for (int i = 0; i < result.length; i++) {
 			try {
 				result[i] = (float) this.func.value( location[i], this.parameters );
-			} catch (FunctionEvaluationException e) {
+			} catch (final FunctionEvaluationException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
@@ -118,11 +121,12 @@ public class FunctionFitModel extends AbstractModel<FunctionFitModel> {
 		return result;
 	}
 
-	public void applyInPlace(float[] location) {
+	@Override
+	public void applyInPlace(final float[] location) {
 		for (int i = 0; i < location.length; i++) {
 			try {
 				location[i] = (float) this.func.value( location[i], this.parameters );
-			} catch (FunctionEvaluationException e) {
+			} catch (final FunctionEvaluationException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
@@ -131,14 +135,16 @@ public class FunctionFitModel extends AbstractModel<FunctionFitModel> {
 
 
 
-	public void set(FunctionFitModel m) {
+	@Override
+	public void set(final FunctionFitModel m) {
 		this.parameters = m.parameters;
 		this.func       = m.func;
 		this.fitter     = m.fitter;
 	}
 
+	@Override
 	public FunctionFitModel copy() {
-		return new FunctionFitModel(parameters.clone(), func, fitter );
+		return new FunctionFitModel( parameters.clone(), func, fitter );
 	}
 
 }
