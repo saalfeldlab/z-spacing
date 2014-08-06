85432af06caeb000d66c78c58b62048989a5d205
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2014-08-06 14:09:57 -0400

    added LUTRealTransformField w/test

---
+++ src/main/java/org/janelia/thickness/lut/LUTRealTransformField.java
@@ -0,0 +1,103 @@
+package org.janelia.thickness.lut;
+
+import net.imglib2.RealLocalizable;
+import net.imglib2.RealPositionable;
+import net.imglib2.img.array.ArrayImg;
+import net.imglib2.img.basictypeaccess.array.DoubleArray;
+import net.imglib2.realtransform.InverseRealTransform;
+import net.imglib2.realtransform.InvertibleRealTransform;
+import net.imglib2.type.numeric.real.DoubleType;
+
+public class LUTRealTransformField extends AbstractLUTRealTransformField {
+
+	
+
+	public LUTRealTransformField(
+			final int numSourceDimensions,
+			final int numTargetDimensions, 
+			final ArrayImg<DoubleType, 
+			DoubleArray> luts) {
+		super(numSourceDimensions, numTargetDimensions, luts);
+	}
+	
+	
+	public LUTRealTransformField(
+			final double[] lut, 
+			final int numSourceDimensions,
+			final int numTargetDimensions, 
+			final long[] dimensions) {
+		super(lut, numSourceDimensions, numTargetDimensions, dimensions);
+	}
+	
+
+	@Override
+	public void apply(final double[] source, final double[] target) {
+		target[0] = source[0];
+		target[1] = source[1];
+		for ( int d = 2; d < source.length; ++d ) {
+			target[d] = applyChecked( source[d], (int) source[0], (int) source[1] );
+		}
+	}
+	
+
+	@Override
+	public void apply(final float[] source, final float[] target) {
+		target[0] = source[0];
+		target[1] = source[1];
+		for ( int d = 2; d < source.length; ++d ) {
+			target[d] = (float) applyChecked( source[d], (int) source[0], (int) source[1] );
+		}
+	}
+	
+
+	@Override
+	public void apply(final RealLocalizable source, final RealPositionable target) {
+		target.setPosition( source );
+		for ( int d = 2; d < source.numDimensions(); ++d ) {
+			target.setPosition( applyChecked( source.getDoublePosition( d ), (int) source.getDoublePosition( 0 ), (int) source.getDoublePosition( 1 ) ), d );
+		}
+	}
+	
+	
+	@Override
+	public void applyInverse(final double[] source, final double[] target) {
+		source[0] = target[0];
+		source[1] = target[1];
+		for ( int d = 2; d < target.length; ++d ) {
+			source[d] = applyInverseChecked( target[d], (int) target[0], (int) target[1] );
+		}
+	}
+	
+
+	@Override
+	public void applyInverse(final float[] source, final float[] target) {
+		source[0] = target[0];
+		source[1] = target[1];
+		for ( int d = 2; d < target.length; ++d ) {
+			source[d] = (float) applyInverseChecked( target[d], (int) target[0], (int) target[1] );
+		}
+	}
+	
+
+	@Override
+	public void applyInverse(final RealPositionable source, final RealLocalizable target) {
+		source.setPosition( target );
+		for ( int d = 2; d < target.numDimensions(); ++d ) {
+			source.setPosition( applyInverseChecked( target.getDoublePosition( d ), (int) target.getDoublePosition( 0 ), (int) target.getDoublePosition( 1 ) ), d );
+		}
+	}
+
+	
+	@Override
+	public InvertibleRealTransform inverse() {
+		return new InverseRealTransform( this );
+	}
+	
+
+	@Override
+	public InvertibleRealTransform copy() {
+		return new LUTRealTransformField( numSourceDimensions, numTargetDimensions, luts );
+	}
+
+
+}
---
+++ src/test/java/org/janelia/thickness/lut/LUTRealTransformFieldTest.java
@@ -0,0 +1,58 @@
+package org.janelia.thickness.lut;
+
+import net.imglib2.RealPoint;
+import net.imglib2.img.array.ArrayImgs;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class LUTRealTransformFieldTest {
+
+	private final double[] sourceDouble = new double[] { 0.0, 0.0, 2.0, 2.0 };
+	private final float[] sourceFloat   = new float[] { 0.0f, 0.0f, 1.0f, 1.0f };
+	private final double[] sourceOffGrid = new double[] { 0.0, 0.0, 2.6, 2.6 };
+	private final RealPoint sourceRealPoint = new RealPoint( sourceOffGrid );
+	private final double[] targetDouble = new double[] { 0.0, 0.0, 5.5, 5.5 };
+	private final float[] targetFloat  = new float[] { 0.0f, 0.0f, 3.5f, 3.5f };
+	private final double[] targetOffGrid = new double[] { 0.0, 0.0, 5.8, 5.8 };
+	private final RealPoint targetRealPoint = new RealPoint( targetOffGrid );
+	
+	private final LUTRealTransformField tf1 = new LUTRealTransformField( 4, 4, ArrayImgs.doubles( new double[] { 1.0, 2.0, 5.5, 6.0 }, 1, 1, 4 ) );
+	private final LUTRealTransformField tf2 = new LUTRealTransformField( 4, 4, ArrayImgs.doubles( new double[] { 1.0, 3.5, 4.5, 5.0 }, 1, 1, 4 ) );
+	
+	private final double[] resultDouble = new double[ 4 ];
+	private final float[] resultFloat = new float[ 4 ];
+	private final RealPoint resultRealPoint = new RealPoint( 4 );
+	
+
+
+	@Test
+	public void test() {
+		tf1.apply( sourceDouble, resultDouble );
+		tf1.apply( sourceRealPoint, resultRealPoint );
+		tf2.apply( sourceFloat, resultFloat );
+		
+		Assert.assertArrayEquals( targetDouble, resultDouble, 0.0 );
+		Assert.assertArrayEquals( targetFloat, resultFloat, 0.0f );
+		for ( int d = 0; d < targetRealPoint.numDimensions(); ++d )
+		{
+			Assert.assertEquals( targetRealPoint.getDoublePosition(d), resultRealPoint.getDoublePosition(d), 0.0 );
+		}
+		
+	}
+	
+	
+	@Test
+	public void testInverse() {
+		tf1.applyInverse( resultDouble, targetDouble );
+		tf1.applyInverse( resultRealPoint, targetRealPoint );
+		tf2.applyInverse( resultFloat, targetFloat );
+		
+		Assert.assertArrayEquals( sourceDouble, resultDouble, 0.0 );
+		Assert.assertArrayEquals( sourceFloat, resultFloat, 0.0f );
+		for ( int d  =  0; d < sourceRealPoint.numDimensions(); ++d ) {
+			Assert.assertEquals(sourceRealPoint.getDoublePosition( d ), resultRealPoint.getDoublePosition( d ), 0.0000000001 );
+		}
+	}
+
+}
