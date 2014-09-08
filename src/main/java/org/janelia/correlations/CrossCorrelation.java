0239264547702e519d2f17ce674c99a88d7bc258
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2014-09-08 13:50:28 -0400

    added CrossCorrelation RandomAccessibleInterval

---
+++ src/main/java/org/janelia/correlations/CrossCorrelation.java
@@ -0,0 +1,289 @@
+package org.janelia.correlations;
+
+import net.imglib2.Cursor;
+import net.imglib2.Interval;
+import net.imglib2.Point;
+import net.imglib2.Positionable;
+import net.imglib2.RandomAccess;
+import net.imglib2.RandomAccessibleInterval;
+import net.imglib2.RealPositionable;
+import net.imglib2.img.array.ArrayImg;
+import net.imglib2.img.array.ArrayImgs;
+import net.imglib2.img.array.ArrayRandomAccess;
+import net.imglib2.img.basictypeaccess.array.BitArray;
+import net.imglib2.img.basictypeaccess.array.DoubleArray;
+import net.imglib2.type.logic.BitType;
+import net.imglib2.type.numeric.RealType;
+import net.imglib2.type.numeric.real.DoubleType;
+import net.imglib2.view.Views;
+
+/**
+ * @author Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
+ *
+ * @param <T>
+ * @param <U>
+ */
+public class CrossCorrelation < T extends RealType< T >, U extends RealType< U > > implements RandomAccessibleInterval<DoubleType> {
+	
+	private final RandomAccessibleInterval<T> img1;
+	private final RandomAccessibleInterval<U> img2;
+	private final ArrayImg< DoubleType, DoubleArray > correlations;
+	private final ArrayImg< BitType, BitArray > calculatedCheck;
+	private final long[] dim;
+	private final long[] r;
+	private final long[] min;
+	private final long[] max;
+	
+	
+
+	public CrossCorrelation(final RandomAccessibleInterval<T> img1,
+			final RandomAccessibleInterval<U> img2,
+			final long[] r ) {
+		super();
+		assert img1.numDimensions() == img2.numDimensions(): "Mismatch in number of dimensions";
+		
+		for ( int d = 0; d < img1.numDimensions(); ++ d ) {
+			assert img1.dimension( d ) == img2.dimension( d ): String.format( "Mismatch in dimension %d", d );
+		}
+		
+		assert r.length == img1.numDimensions() || r.length == 1: "Mismatch in number of dimensions and radii";
+		
+		this.img1 = img1;
+		this.img2 = img2;
+		
+		this.dim = new long[ img1.numDimensions() ];
+		img1.dimensions( dim );
+		this.min = new long[ dim.length ];
+		this.max = new long[ dim.length ];
+		for ( int d = 0; d < dim.length; ++d ) {
+			this.min[d] = 0;
+			this.max[d] = dim[d] - 1;
+		}
+		
+		this.correlations    = ArrayImgs.doubles( dim );
+		this.calculatedCheck = ArrayImgs.bits( dim );
+		
+		if ( r.length == 1 ) {
+			this.r = new long[ this.dim.length ];
+			for (int i = 0; i < this.r.length; i++) {
+				this.r[ i ] = r[ 0 ];
+			}
+		} else		
+			this.r = r.clone();
+		
+	}
+	
+	public class CrossCorrelationRandomAaccess extends Point implements RandomAccess< DoubleType > {
+		
+		private final ArrayRandomAccess< BitType > checkAccess;
+		private final ArrayRandomAccess< DoubleType > correlationsAccess;
+		
+		private final long[] intervalMin;
+		private final long[] intervalMax;
+	
+		private CrossCorrelationRandomAaccess(final long[] position,
+				final ArrayRandomAccess<BitType> checkAccess,
+				final ArrayRandomAccess<DoubleType> correlationsAccess,
+				final long[] intervalMin, final long[] intervalMax) {
+			super(position);
+			this.checkAccess = checkAccess;
+			this.correlationsAccess = correlationsAccess;
+			this.intervalMin = intervalMin;
+			this.intervalMax = intervalMax;
+		}
+
+		public CrossCorrelationRandomAaccess() {
+			super( dim.length );
+			this.checkAccess        = calculatedCheck.randomAccess();
+			this.correlationsAccess = correlations.randomAccess();
+			
+			intervalMin = new long[ dim.length ];
+			intervalMax = new long[ dim.length ];
+		}
+
+		@Override
+		public DoubleType get() {
+			checkAccess.setPosition( this.position );
+			correlationsAccess.setPosition( this.position );
+			
+			final DoubleType currVal = correlationsAccess.get();
+			
+			if ( checkAccess.get().get() == false ) {
+				
+				for ( int d = 0; d < this.n; ++d ) {
+					intervalMin[d] = Math.max( 0,      this.position[d] - r[d] );
+					intervalMax[d] = Math.min( max[d], this.position[d] + r[d] );
+				}
+				
+				currVal.set( calculateNormalizedCrossCorrelation( 
+						Views.interval( img1, intervalMin, intervalMax ),
+						Views.interval( img2, intervalMin, intervalMax )
+						) 
+						);
+			}
+			
+			return currVal;
+			
+		}
+
+		@Override
+		public CrossCorrelationRandomAaccess copy() {
+			return new CrossCorrelationRandomAaccess( this.position.clone(), 
+					checkAccess.copy(), 
+					correlationsAccess.copy(), 
+					intervalMin.clone(), 
+					intervalMax.clone());
+		}
+
+		@Override
+		public CrossCorrelationRandomAaccess copyRandomAccess() {
+			return copy();
+		}
+		
+	}
+	
+	public double calculateNormalizedCrossCorrelation( final RandomAccessibleInterval< T > i1, final RandomAccessibleInterval< U > i2 ) {
+		double cc = 0.0;
+		
+		long nElements = 1;
+		for ( int d = 0; d < i1.numDimensions(); ++d ) {
+			nElements *= i1.dimension( d );
+		}
+		
+		final double nElementsDouble = nElements;
+		
+		final double mean1 = calculateSum( i1 ) / nElementsDouble;
+		final double mean2 = calculateSum( i2 ) / nElementsDouble;
+		final double var1 = calculateSumOfSquaredDifferences( i1, mean1 ) / nElementsDouble;
+		final double var2 = calculateSumOfSquaredDifferences( i2, mean2 ) / nElementsDouble;
+		
+		final Cursor<T> c1 = Views.flatIterable( i1 ).cursor();
+		final Cursor<U> c2 = Views.flatIterable( i2 ).cursor();
+		
+		while( c1.hasNext() ) {
+			cc += ( c1.next().getRealDouble() - mean1 ) * ( c2.next().getRealDouble() - mean2 );
+		}
+		
+		return cc / ( Math.sqrt( var1 ) * Math.sqrt( var2 ) * nElementsDouble );
+	}
+	
+	
+	public < V extends RealType<V> > double  calculateSum( final RandomAccessibleInterval< V > i ) {
+		double sum = 0.0;
+		final Cursor< V > c = Views.flatIterable( i ).cursor();
+		while ( c.hasNext() ) {
+			sum += c.next().getRealDouble();
+		}
+		return sum;
+	}
+	
+	
+	public < V extends RealType<V> > double calculateSumOfSquaredDifferences( final RandomAccessibleInterval< V > i, final double mean ) {
+		double sum = 0.0;
+		final Cursor< V > c = Views.flatIterable( i ).cursor();
+		double tmpDiff;
+		while( c.hasNext() ) {
+			tmpDiff = c.next().getRealDouble() - mean;
+			sum    += tmpDiff*tmpDiff;
+		}
+		return sum;
+	}
+
+
+	@Override
+	public CrossCorrelationRandomAaccess randomAccess() {
+		return new CrossCorrelationRandomAaccess();
+	}
+
+	@Override
+	public CrossCorrelationRandomAaccess randomAccess(final Interval interval) {
+		return randomAccess();
+	}
+
+	@Override
+	public int numDimensions() {
+		return this.dim.length;
+	}
+
+	@Override
+	public long min(final int d) {
+		return this.min[ d ];
+	}
+
+	@Override
+	public void min(final long[] min) {
+		for (int i = 0; i < min.length; i++) {
+			min[i] = this.min[i];
+		}
+	}
+
+	@Override
+	public void min(final Positionable min) {
+		min.setPosition( this.min );
+	}
+
+	@Override
+	public long max(final int d) {
+		// TODO Auto-generated method stub
+		return this.max[d];
+	}
+
+	@Override
+	public void max(final long[] max) {
+		for (int i = 0; i < max.length; i++) {
+			max[i] = this.max[i];
+		}
+	}
+
+	@Override
+	public void max(final Positionable max) {
+		max.setPosition(this.max);
+	}
+
+	@Override
+	public double realMin(final int d) {
+		return min(d);
+	}
+
+	@Override
+	public void realMin(final double[] min) {
+		for (int i = 0; i < min.length; i++) {
+			min[i] = this.min[i];
+		}
+	}
+
+	@Override
+	public void realMin(final RealPositionable min) {
+		min( min );
+	}
+
+	@Override
+	public double realMax(final int d) {
+		return max( d );
+	}
+
+	@Override
+	public void realMax(final double[] max) {
+		for (int i = 0; i < max.length; i++) {
+			max[i] = this.max[i];
+		}
+	}
+
+	@Override
+	public void realMax(final RealPositionable max) {
+		max( max );
+	}
+
+	@Override
+	public void dimensions(final long[] dimensions) {
+		for (int i = 0; i < dimensions.length; i++) {
+			dimensions[i] = this.dim[i];
+		}
+	}
+
+	@Override
+	public long dimension(final int d) {
+		return this.dim[d];
+	}
+
+}
---
+++ src/test/java/org/janelia/correlations/CrossCorrelationTest.java
@@ -0,0 +1,79 @@
+package org.janelia.correlations;
+
+import java.util.Random;
+
+import net.imglib2.RandomAccess;
+import net.imglib2.img.array.ArrayCursor;
+import net.imglib2.img.array.ArrayImg;
+import net.imglib2.img.array.ArrayImgs;
+import net.imglib2.img.basictypeaccess.array.DoubleArray;
+import net.imglib2.type.numeric.real.DoubleType;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CrossCorrelationTest {
+	
+	private final long[] dim = new long[] { 4,5 };
+	private final long[] randomDim = new long[] { (long) 1e6 };
+	private final ArrayImg< DoubleType, DoubleArray > img1 = ArrayImgs.doubles( dim );
+	private final ArrayImg< DoubleType, DoubleArray > img2 = ArrayImgs.doubles( dim );
+	private final ArrayImg< DoubleType, DoubleArray > img3 = ArrayImgs.doubles( dim );
+	private final ArrayImg< DoubleType, DoubleArray > img4 = ArrayImgs.doubles( randomDim );
+	private final ArrayImg< DoubleType, DoubleArray > img5 = ArrayImgs.doubles( randomDim );
+	private final Random rng = new Random( 100 );
+
+	@Before
+	public void setUp() throws Exception {
+		
+		final ArrayCursor<DoubleType> c1 = img1.cursor();
+		final ArrayCursor<DoubleType> c2 = img2.cursor();
+		final ArrayCursor<DoubleType> c3 = img3.cursor();
+		final ArrayCursor<DoubleType> c4 = img4.cursor();
+		final ArrayCursor<DoubleType> c5 = img5.cursor();
+		
+		for ( int i = 1; c1.hasNext(); ++i ) {
+			c1.next().set( i );
+			c2.next().set( 0.5*i + 1 );
+			c3.next().set( -0.5*i );
+		}
+		/*
+		 * mean1: 10.50
+		 * mean2:  6.25
+		 * mean3: -5.25
+		 */
+		while( c4.hasNext() ) {
+			c4.next().set( rng.nextDouble() );
+			c5.next().set( rng.nextDouble() );
+		}
+		
+	}
+
+	@Test
+	public void test() {
+		// correlation == 1
+		final CrossCorrelation<DoubleType, DoubleType> cc1 = new CrossCorrelation<DoubleType, DoubleType>(img1, img2, new long[] { 2, 2 } );
+		final RandomAccess<DoubleType> ra1 = cc1.randomAccess();
+		ra1.setPosition( new int[] { 2, 2 } );
+		Assert.assertEquals( 1.0, ra1.get().get(), 0.00000000001 );
+		
+		// correlation == -1
+		final CrossCorrelation<DoubleType, DoubleType> cc2 = new CrossCorrelation<DoubleType, DoubleType>(img1, img3, new long[] { 2, 2 } );
+		final RandomAccess<DoubleType> ra2 = cc2.randomAccess();
+		ra2.setPosition( new int[] { 2, 2 } );
+		Assert.assertEquals( -1.0, ra2.get().get(), 0.00000000001 );
+		
+		// correlation ~~ 0
+		final CrossCorrelation< DoubleType, DoubleType > cc3 = new CrossCorrelation<DoubleType, DoubleType>(img4, img5, randomDim );
+		final RandomAccess< DoubleType > ra3 = cc3.randomAccess();
+		ra3.setPosition( new long[] { randomDim[0] / 2 } );
+		Assert.assertEquals( 0.0, ra3.get().get(), 1e-3 ); 
+		/*
+		 * 1e-3 is strongly dependent on number of elemetns in img4/img5
+		 * and might also be dependent on seed 
+		 */
+	}
+
+}
+
