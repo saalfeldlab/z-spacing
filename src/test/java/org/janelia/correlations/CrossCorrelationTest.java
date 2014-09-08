03be2ac644e8efb2ba32042732631072cc655cd5
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2014-09-08 13:53:45 -0400

    check that correlation value stays the same when called repeatedly

--- src/test/java/org/janelia/correlations/CrossCorrelationTest.java
+++ src/test/java/org/janelia/correlations/CrossCorrelationTest.java
@@ -52,27 +52,38 @@ public class CrossCorrelationTest {
 
 	@Test
 	public void test() {
+		double val1;
+		double val2;
 		// correlation == 1
 		final CrossCorrelation<DoubleType, DoubleType> cc1 = new CrossCorrelation<DoubleType, DoubleType>(img1, img2, new long[] { 2, 2 } );
 		final RandomAccess<DoubleType> ra1 = cc1.randomAccess();
 		ra1.setPosition( new int[] { 2, 2 } );
-		Assert.assertEquals( 1.0, ra1.get().get(), 0.00000000001 );
+		val1 = ra1.get().get();
+		val2 = ra1.get().get();
+		Assert.assertEquals( 1.0, val1, 0.00000000001 );
+		Assert.assertEquals( val1, val2, 0.0 );
 		
 		// correlation == -1
 		final CrossCorrelation<DoubleType, DoubleType> cc2 = new CrossCorrelation<DoubleType, DoubleType>(img1, img3, new long[] { 2, 2 } );
 		final RandomAccess<DoubleType> ra2 = cc2.randomAccess();
 		ra2.setPosition( new int[] { 2, 2 } );
-		Assert.assertEquals( -1.0, ra2.get().get(), 0.00000000001 );
+		val1 = ra2.get().get();
+		val2 = ra2.get().get();
+		Assert.assertEquals( -1.0, val1, 0.00000000001 );
+		Assert.assertEquals( val1,  val2, 0.0 );
 		
 		// correlation ~~ 0
 		final CrossCorrelation< DoubleType, DoubleType > cc3 = new CrossCorrelation<DoubleType, DoubleType>(img4, img5, randomDim );
 		final RandomAccess< DoubleType > ra3 = cc3.randomAccess();
 		ra3.setPosition( new long[] { randomDim[0] / 2 } );
-		Assert.assertEquals( 0.0, ra3.get().get(), 1e-3 ); 
+		val1 = ra3.get().get();
+		val2 = ra3.get().get();
+		Assert.assertEquals( 0.0, val1, 1e-3 ); 
 		/*
 		 * 1e-3 is strongly dependent on number of elemetns in img4/img5
 		 * and might also be dependent on seed 
 		 */
+		Assert.assertEquals( val1, val2, 0.0 );
 	}
 
 }
