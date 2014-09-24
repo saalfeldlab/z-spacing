41451e8aff6e0bb9c7780987b139c939c8fa1999
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2014-09-24 14:35:00 -0400

    added IntegerToIntegerConverter

---
+++ src/main/java/org/janelia/utility/IntegerToIntegerConverter.java
@@ -0,0 +1,13 @@
+package org.janelia.utility;
+
+import net.imglib2.converter.Converter;
+import net.imglib2.type.numeric.IntegerType;
+
+public class IntegerToIntegerConverter< A extends IntegerType< A >, B extends IntegerType< B > > implements Converter<A, B> {
+
+	@Override
+	public void convert(final A input, final B output) {
+		output.setInteger( input.getIntegerLong() );
+	}
+
+}
