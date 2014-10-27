93aa22da7be0fcb3335ae5cd7ef12b2d8a2e6850
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2014-10-27 15:42:00 -0400

    added copy with Converter for arbitrary types

--- src/main/java/org/janelia/utility/CopyFromIntervalToInterval.java
+++ src/main/java/org/janelia/utility/CopyFromIntervalToInterval.java
@@ -2,6 +2,7 @@ package org.janelia.utility;
 
 import net.imglib2.Cursor;
 import net.imglib2.RandomAccessibleInterval;
+import net.imglib2.converter.Converter;
 import net.imglib2.type.numeric.IntegerType;
 import net.imglib2.type.numeric.RealType;
 import net.imglib2.view.Views;
@@ -42,6 +43,21 @@ public class CopyFromIntervalToInterval {
 		
 		while ( sourceCursor.hasNext() ) {
 			targetCursor.next().setReal( sourceCursor.next().getRealDouble() );
+		}
+	}
+	
+	public static < T, U > void copy( final RandomAccessibleInterval<T> source, final RandomAccessibleInterval<U> target, final Converter< T, U > converter ) {
+		assert source.numDimensions() == target.numDimensions(): "Dimension mismatch";
+		for ( int d = 0; d < source.numDimensions(); ++ d ) {
+			assert target.dimension( d ) == source.dimension( d ): "Dimension mismatch";
+		}
+		final Cursor<T> sourceCursor = Views.flatIterable( source ).cursor();
+		final Cursor<U> targetCursor = Views.flatIterable( target ).cursor();
+		
+		while ( sourceCursor.hasNext() ) {
+			final T s = sourceCursor.next();
+			final U t = targetCursor.next();
+			converter.convert( s, t );
 		}
 	}
 
