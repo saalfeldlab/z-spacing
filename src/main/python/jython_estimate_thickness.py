85e7a563ef44f17bc2b375c129432f049ee13ecb
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2014-10-28 18:04:49 -0400

    removed unnecessary code and comments

--- src/main/python/jython_estimate_thickness.py
+++ src/main/python/jython_estimate_thickness.py
@@ -73,141 +73,9 @@ def make_sure_path_exists(path):
         if exception.errno != errno.EEXIST:
             raise
 
-class Fitter(object):
-    def __init__(self, offset, initialGuess, function, stepsize, fittingRange):
-        # self.fitter       = StackFitterNoUncertainty(offset, initialGuess, function)
-        self.stepSize     = stepSize
-        self.fittingRange = fittingRange
 
-
-    def fit(self, image):
-        pass
-
-
-class FitterFactory(object):
-    def create(self, offset, initialGuess, function, stepsize, fittingRange):
-        return Fitter(offset, initialGuess, function, stepsize, fittingRange)
-
-    
-
-
-class CorrelationsCreator(object):
-    def __init__(self, imagePlus, radius=[8, 8], offset=[0,0]):
-        self.correlations = {}
-        self.radius = radius
-        self.imagePlus = imagePlus
-        self.offset = [0,0]
-
-    def correlate(self, index1, index2):
-        if (index1, index2) in self.correlations.keys():
-            pass 
-        elif (index2, index1) in self.correlations.keys():
-            self.correlations[(index1, index2)] = self.correlations[(index2, index1)]
-        else:
-            self.correlations[(index1, index2)] = self.computeCorrelation(index1, index2)
-        
-        return self.correlations[(index1, index2)]
-
-    def computeCorrelation(self, index1, index2):
-        ip1  = self.imagePlus.getStack().getProcessor(index1).convertToFloatProcessor()
-        ip2  = self.imagePlus.getStack().getProcessor(index2).convertToFloatProcessor()
-        pmcc = BlockPMCC(ip1, ip2, *self.offset)
-        pmcc.rSignedSquare(self.radius[0], self.radius[1])
-        tp = pmcc.getTargetProcessor()
-        tp.min(0.0)
-        return tp
-        
-
-    def correlateAll(self):
-        size = self.imagePlus.getStack().getSize()
-        # for index1 in xrange(1, size+1):
-        #     for index2 in xrange(1, size+1):
-        #         self.correlate(index1, index2)
-
-        return self.correlateAllWithinRange(size)
-
-        # return self.correlations
-
-    def correlateAllWithinRange(self, maximumRange):
-        size = self.imagePlus.getStack().getSize()
-        for index1 in xrange(1, size+1):
-            for index2 in xrange(1, size+1):
-                if int(math.fabs(index2-index1)) > maximumRange:
-                    continue
-                self.correlate(index1, index2)
-                
-        return self. correlations
-
-    def toStack(self, index):
-        size = self.imagePlus.getStack().getSize()
-        width = self.imagePlus.getStack().getWidth()
-        height = self.imagePlus.getStack().getHeight()
-        resultStack = ImageStack(width, height, size)
-        for k, v in self.correlations.iteritems():
-            if k[0] == index:
-                resultStack.setProcessor(v, k[1])
-        return resultStack, (1, size)
-        # return self.toStackRange(index, size/2)
-
-    def toStackRange(self, index, stackRange):
-        upperEnd = min(index + stackRange, self.imagePlus.getStack().getSize())
-        lowerEnd = max(index - stackRange, 1)
-        size = upperEnd - lowerEnd + 1 # min(self.imagePlus.getStack().getSize(), 2*stackRange+1)
-        width = self.imagePlus.getStack().getWidth()
-        height = self.imagePlus.getStack().getHeight()
-        resultStack = ImageStack(width, height, size)
-        for k, v in self.correlations.iteritems():
-            if k[0] == index:
-                # if int(math.fabs(k[1] - index)) > stackRange:
-                # continue
-                if k[1] > upperEnd or k[1] < lowerEnd:
-                    continue
-                putIndex = k[1] + 1 - lowerEnd
-                resultStack.setProcessor(v, putIndex)
-        return resultStack, (lowerEnd, upperEnd)
-
-
-class Bucket(object):
-    def __init__(self, image, fitterFactory, stepSize, fittingRange):
-        self.buckets = {}
-        self.image   = image
-        self.fitterFactory  = fitterFactory
-        self.stepSize = stepSize
-        self.fittingRange = fittingRange
-
-    def createBucketsAtScaleAndRange(radius, stackRange):
-        co = CorrelationsCreator(self.image, radius=radius)
-        co.correlateAllWithinRange(stackRange)
-        result = []
-        for idx in xrange(1, self.image.getStack().getSize() + 1):
-            stack, interval = co.toStackRange(idx, stackRange)
-            offset = idx - interval[0]
-            initialGuess = [1.0]
-            function = SingleParameterBellCurve()
-            fitter = fitterFactory.create(offset, initialGuess, function, self.stepSize, self.fittingRange)            
-            result.append(fitter.fit(stack, interval))
-        self.buckets[radius] = result
-        return result
-
-
-def parseOptionsFromFile( filename ):
-
-    defaultValues = {
-        
-    }
-    
-    result = Options()
-    visitorOptions = {}
 
 if __name__ == "__main__":
-
-    # import optparse
-
-    # parser = optparse.OptionParser()
-    # parser.add_option('--config', '-c', default='/dev/null', help='Config file that stores parameters and filenames.')
-
-    # options, args = parser.parse_args()
-
 
     t0 = time.time()
     print t0 - t0
@@ -315,29 +183,7 @@ if __name__ == "__main__":
         this_file_name = os.path.realpath( inspect.getfile( lambda : None ) ) # inspect.getfile requires method, class, ... as input and returns the file in which input was defined
         shutil.copyfile( this_file_name, '%s/%s' % ( home.rstrip('/'), this_file_name.split('/')[-1] ) )
         
-        # START DEPRECATED correlation calculation
-        # cc = CorrelationsCreator(img, [img.getWidth(), img.getHeight()])
-        # cc.correlateAllWithinRange( correlationRange )
-             
-        # t1 = time.time()
-        # print t1 - t0
-             
-        # # options.coordinateUpdateRegularizerWeight = 0.1
-        # # options.fitIntervalLength = 3
-        # # options.stride            = 2
-        # # options.fitterFactory     = StackFitterNoUncertaintyFactory([1.0])
-             
-        # co = CorrelationsObject()
-             
-        # coordinateBase = ArrayList()
-             
-        # positions = TreeMap();
-        # positions.put( ConstantPair( Long(0), Long(0) ), ArrayList() );
-             
         startingCoordinates = []
-             
-        # t2 = time.time()
-        # print t2 - t0
 
         start = 1
         stop  = img.getStack().getSize()
@@ -346,26 +192,6 @@ if __name__ == "__main__":
         if False and stackMax != None:
             stop  = stackMax
         startingCoordinates = range( start - 1, stop )
-        # for i in xrange( start, stop + 1 ):
-        #     stackRange, interval = cc.toStackRange( i, correlationRange )
-             
-        #     out = ImagePlus('test_%02d' % i, stackRange)
-        #     # out.show()
-             
-        #     meta                = CorrelationsObject.Meta()
-        #     meta.zPosition      = i
-        #     meta.zCoordinateMin = interval[0]
-        #     meta.zCoordinateMax = interval[1] + 1 # exclusive
-             
-        #     adapter = ImagePlusAdapter.wrap(out)
-        #     co.addCorrelationImage(meta.zPosition, adapter, meta)
-             
-        #     coordinateBase.add( float(i) )
-             
-        #     startingCoordinates.append( float(i-1) )
-        # END DEPRECATED correlation calculation
-
-        # START create CorrelationsObject with factory
 
         co = SparseCorrelationsObject()
         t0Prime = time.time()
@@ -423,8 +249,6 @@ if __name__ == "__main__":
         bp = home + "/render/render_%02d.tif"
         make_sure_path_exists( bp )
         hyperSlices = ArrayList()
-        # hyperSlice = Views.hyperSlice( ImagePlusImgs.from( imgSource ), 1,  345 )
-        # ImageJFunctions.show( hyperSlice )
              
         renderTracker = ApplyTransformToImagesAndAverageVisitor( bp, # base path
                                                                  FloorInterpolatorFactory(), # interpolation
@@ -480,42 +304,4 @@ if __name__ == "__main__":
         # options.multiplierGenerationRegularizerWeight = <value>
         # or equivalent
         result = inference.estimateZCoordinates( 0, 0, startingCoordinates, matrixTracker, options )
-             
-             
-        # array = jarray.zeros( result.dimension(0), 'd' )
-        # cursor = result.cursor()
-        # for i in xrange(result.dimension(0) ):
-        #     array[i] = scale * cursor.next().get()
-             
-        # lutTransform = SingleDimensionLUTRealTransform( array, 3, 3, 2 )
-             
-        # resultImage = ImagePlusImgs.unsignedBytes( imgSource.getWidth(), imgSource.getHeight(), int(scale) * stack.getSize() )
-        # interpolated = Views.interpolate( Views.extendValue( ImagePlusImgs.from(imgSource), DoubleType( Double.NaN ) ), FloorInterpolatorFactory() )
-        # transformed =  Views.interval( RealViews.transform( interpolated, lutTransform ), resultImage )
-             
-        # print type(transformed)
-        # print type(resultImage)
-        # CopyFromIntervalToInterval.copy( transformed, resultImage )
-             
-             
-        # ImageJFunctions.show( resultImage )
-             
-             
-             
-             
-        # t4 = time.time()
-        # print t4 - t0
-        # print
-        # for r in result:
-        #     print r
-             
-             
-             
-        # metaMap = co.getMetaMap()
-        # fitMap = co.getFitMap()
-        # for entryset in fitMap.entrySet():
-        #     show = ImgLib2Display.copyToImagePlus(entryset.getValue())
-        #     show.show()
-        #     print entryset
-             
  
