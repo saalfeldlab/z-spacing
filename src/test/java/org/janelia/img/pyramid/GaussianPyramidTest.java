2f63d4d837d915f053f4b32cfe2bb2a579fecdb4
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2014-10-30 10:33:53 -0400

    do not show images in test

--- src/test/java/org/janelia/img/pyramid/GaussianPyramidTest.java
+++ src/test/java/org/janelia/img/pyramid/GaussianPyramidTest.java
@@ -1,6 +1,5 @@
 package org.janelia.img.pyramid;
 
-import ij.ImageJ;
 import ij.ImagePlus;
 
 import java.io.IOException;
@@ -10,7 +9,6 @@ import net.imglib2.img.ImagePlusAdapter;
 import net.imglib2.img.Img;
 import net.imglib2.img.array.ArrayImgFactory;
 import net.imglib2.img.array.ArrayImgs;
-import net.imglib2.img.display.imagej.ImageJFunctions;
 import net.imglib2.img.imageplus.FloatImagePlus;
 import net.imglib2.type.numeric.real.DoubleType;
 import net.imglib2.type.numeric.real.FloatType;
@@ -34,12 +32,7 @@ public class GaussianPyramidTest {
 		final ImagePlus original  = new ImagePlus( "http://d3sdoylwcs36el.cloudfront.net/VEN-virtual-enterprise-network-business-opportunities-small-fish_id799929_size485.jpg" );
 		final ImagePlus converted = new ImagePlus( "", original.getProcessor().convertToFloat() );
 		final FloatImagePlus<FloatType> wrapped = ImagePlusAdapter.wrapFloat( converted );
-		new ImageJ();
-		ImageJFunctions.show( wrapped );
 		final GaussianPyramid<FloatType> pyr = new GaussianPyramid<FloatType>( wrapped, 0.5, 2.0, new ArrayImgFactory<FloatType>());
-		for ( int index = 0; index < 2; ++index ) { // pyr.getMaxLevel(); ++index ) {
-			ImageJFunctions.show( pyr.get( index ) );
-		}
 	}
 
 }
