903ed61efe6a25044fa3987f784ad11b3e963f8b
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2014-11-04 10:57:17 -0500

    use difference of coordinates rather than coordinates for clustering

--- src/main/java/org/janelia/thickness/cluster/ClusteringCategorizer.java
+++ src/main/java/org/janelia/thickness/cluster/ClusteringCategorizer.java
@@ -3,6 +3,8 @@
  */
 package org.janelia.thickness.cluster;
 
+import ij.IJ;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -30,13 +32,20 @@ public class ClusteringCategorizer< T extends Clusterer< ZPositionWrapper > > im
 
 	@Override
 	public double[][] getLabels(final double[] coordinates) {
-		final List<ZPositionWrapper> list = ZPositionWrapper.toList( coordinates );
+		IJ.log( "before calculating distances" );
+		final double[] distance = new double[ coordinates.length ];
+		for (int i = 0; i < distance.length-1; i++) {
+			distance[i] = coordinates[i+1] - coordinates[i];
+		}
+		distance[distance.length-1] = distance[distance.length-2];
+		IJ.log( "after calculating distances" );
+		final List<ZPositionWrapper> list = ZPositionWrapper.toList( distance );
 		final List<? extends Cluster<ZPositionWrapper>> result = this.clusterer.cluster( list );
 		if ( (result.get(0) instanceof CentroidCluster<?> ) ) {
 			final ArrayList<CentroidCluster<ZPositionWrapper>> centroids = new ArrayList< CentroidCluster< ZPositionWrapper > >();
 			for ( final Cluster<ZPositionWrapper> r : result )
 				centroids.add( (CentroidCluster< ZPositionWrapper >) r );
-			return Convenience.getSoftAssignments( coordinates, centroids, clusterer.getDistanceMeasure() );
+			return Convenience.getSoftAssignments( distance, centroids, clusterer.getDistanceMeasure() );
 		} else
 			return null;
 		
