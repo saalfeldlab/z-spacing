969752cc8c6b6f347813d62b137e8f625e7a2234
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2014-11-07 16:22:18 -0500

    no logging for clustering optimizer

--- src/main/java/org/janelia/thickness/cluster/ClusteringCategorizer.java
+++ src/main/java/org/janelia/thickness/cluster/ClusteringCategorizer.java
@@ -3,8 +3,6 @@
  */
 package org.janelia.thickness.cluster;
 
-import ij.IJ;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -32,13 +30,11 @@ public class ClusteringCategorizer< T extends Clusterer< ZPositionWrapper > > im
 
 	@Override
 	public double[][] getLabels(final double[] coordinates) {
-		IJ.log( "before calculating distances" );
 		final double[] distance = new double[ coordinates.length ];
 		for (int i = 0; i < distance.length-1; i++) {
 			distance[i] = coordinates[i+1] - coordinates[i];
 		}
 		distance[distance.length-1] = distance[distance.length-2];
-		IJ.log( "after calculating distances" );
 		final List<ZPositionWrapper> list = ZPositionWrapper.toList( distance );
 		final List<? extends Cluster<ZPositionWrapper>> result = this.clusterer.cluster( list );
 		if ( (result.get(0) instanceof CentroidCluster<?> ) ) {
