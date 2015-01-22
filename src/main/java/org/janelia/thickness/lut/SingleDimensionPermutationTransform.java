0ec028f8cd7f916b183460cb8690740cb914d232
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2015-01-22 14:04:18 -0500

    added SingleDimensionPermutationTransform w/test

---
+++ src/main/java/org/janelia/thickness/lut/SingleDimensionPermutationTransform.java
@@ -0,0 +1,91 @@
+/**
+ * 
+ */
+package org.janelia.thickness.lut;
+
+import net.imglib2.Localizable;
+import net.imglib2.Positionable;
+
+/**
+ * @author Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
+ *
+ * Apply bijective permutation to one coordinate axis only.
+ *
+ */
+public class SingleDimensionPermutationTransform extends
+		AbstractPerumtationTransform {
+	
+	final protected int numSourceDimensions;
+	final protected int numTargetDimensions;
+	final protected int d;
+
+	/**
+	 * @param lut
+	 * @param numSourceDimensions dimensionality of source
+	 * @param numTargetDimensions dimensionality of target
+	 * @param d dimension which shall be transformed. Must be smaller than {@link #numSourceDimensions} and {@link #numTargetDimensions}
+	 */
+	public SingleDimensionPermutationTransform(
+			final int[] lut,
+			final int numSourceDimensions, 
+			final int numTargetDimensions,
+			final int d) {
+		super(lut);
+		assert d > 0 && d < numTargetDimensions && d < numSourceDimensions;
+		this.numSourceDimensions = numSourceDimensions;
+		this.numTargetDimensions = numTargetDimensions;
+		this.d = d;
+	}
+
+	@Override
+	public void applyInverse(final long[] source, final long[] target) {
+		System.arraycopy( target, 0, source, 0, this.numSourceDimensions );
+		source[ d ] = applyInverse( (int) target[ d] );
+	}
+
+	@Override
+	public void applyInverse(final int[] source, final int[] target) {
+		System.arraycopy( target, 0, source, 0, this.numSourceDimensions );
+		source[ d ] = applyInverse( target[ d] );
+	}
+
+	@Override
+	public void applyInverse(final Positionable source, final Localizable target) {
+		source.setPosition( target );
+		source.setPosition( applyInverse( target.getIntPosition( d ) ), d );
+	}
+
+	@Override
+	public SingleDimensionPermutationTransform inverse() {
+		return new SingleDimensionPermutationTransform( inverseLut, numSourceDimensions, numTargetDimensions, d );
+	}
+
+	@Override
+	public int numSourceDimensions() {
+		return this.numSourceDimensions;
+	}
+
+	@Override
+	public int numTargetDimensions() {
+		return this.numTargetDimensions;
+	}
+
+	@Override
+	public void apply(final long[] source, final long[] target) {
+		System.arraycopy( source, 0, target, 0, this.numTargetDimensions );
+		target[ d ] = apply( (int) source[ d ] );
+	}
+
+	@Override
+	public void apply(final int[] source, final int[] target) {
+		System.arraycopy( source, 0, target, 0, this.numTargetDimensions );
+		target[ d ] = apply( source[ d ] );
+	}
+
+	@Override
+	public void apply(final Localizable source, final Positionable target) {
+		target.setPosition( source );
+		target.setPosition( apply( source.getIntPosition( d ) ), d );
+	}
+
+}
---
+++ src/test/java/org/janelia/thickness/lut/SingleDimensionPermutationTransformTest.java
@@ -0,0 +1,128 @@
+/**
+ * 
+ */
+package org.janelia.thickness.lut;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+
+import net.imglib2.Cursor;
+import net.imglib2.img.array.ArrayCursor;
+import net.imglib2.img.array.ArrayImg;
+import net.imglib2.img.array.ArrayImgs;
+import net.imglib2.img.array.ArrayRandomAccess;
+import net.imglib2.img.basictypeaccess.array.IntArray;
+import net.imglib2.type.numeric.integer.IntType;
+import net.imglib2.view.TransformView;
+import net.imglib2.view.Views;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
+ *
+ */
+public class SingleDimensionPermutationTransformTest {
+	
+	private final Random rng = new Random();
+	private final int width       = 100;
+	private final int height      = 200;
+	private final long[] dim      = new long[] { width, height };
+	private final int d           = 1;
+	private final int size        = (int) dim[ d ];
+	private final int nDim        = dim.length;
+	private final int[] lut       = new int[ size ];
+	private final int[] inv       = new int[ size ];
+	private final int nRandomReps = 1000;
+	
+	private final ArrayImg< IntType, IntArray > img = ArrayImgs.ints( dim );
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+		final ArrayList<Integer> al = new ArrayList< Integer >();
+		for ( int i = 0; i < size; ++i )
+			al.add( i );
+		
+		Collections.shuffle( al );
+		
+		for ( int i = 0;  i < lut.length; ++i ) {
+			lut[ i ]        = al.get( i );
+			inv[ lut[ i ] ] = i;
+		}
+		
+		for ( final IntType l : img )
+			l.set( rng.nextInt() );
+	}
+
+	
+	@Test
+	public <T> void test() {
+		final SingleDimensionPermutationTransform transform = new SingleDimensionPermutationTransform( lut, nDim, nDim, d );
+		final SingleDimensionPermutationTransform inverse   = transform.inverse();
+
+		final TransformView< IntType > transformed = new TransformView< IntType >( img, transform );
+		final TransformView< IntType > inversed    = new TransformView< IntType >( transformed, inverse );
+		
+		final ArrayCursor< IntType > ref = img.cursor();
+		final Cursor< IntType > res      = Views.flatIterable( Views.interval( inversed, img ) ).cursor();
+		while( ref.hasNext() )
+			Assert.assertEquals( ref.next().get(), res.next().get() );
+
+		final ArrayRandomAccess<IntType> raFwd = img.randomAccess();
+		final ArrayRandomAccess<IntType> raBck = img.randomAccess();
+		final ArrayRandomAccess<IntType> ra    = img.randomAccess();
+		
+		final long[] fwdLong = new long[ nDim ];
+		final long[] bckLong = new long[ nDim ];
+		
+		final int[] fwdInt = new int[ nDim ];
+		final int[] bckInt = new int[ nDim ];
+		
+		for ( int i = 0; i < nRandomReps; ++i ) {
+			final int x = rng.nextInt( width );
+			final int y = rng.nextInt( width );
+			final int[] xyInt   = new int[] { x, y };
+			final long[] xyLong = new long[] { x, y }; 
+			ra.setPosition( xyInt );
+			
+			transform.apply( xyInt, fwdInt );
+			transform.apply( xyLong, fwdLong );
+			transform.apply( ra, raFwd );
+			
+			transform.applyInverse( bckInt, xyInt );
+			transform.applyInverse( bckLong, xyLong );
+			transform.applyInverse( raBck, ra );
+			
+			for ( int d = 0; d < nDim; ++d ) {
+				final int fwdVal;
+				final int bckVal;
+				if ( d == this.d ) {
+					fwdVal = lut[ xyInt[ d ] ];
+					bckVal = inv[ xyInt[ d ] ];
+				} else {
+					fwdVal = xyInt[ d ];
+					bckVal = xyInt[ d ];
+				}
+
+				Assert.assertEquals( fwdVal, fwdInt[d] );
+				Assert.assertEquals( bckVal, bckInt[d] );
+				
+				Assert.assertEquals( fwdVal, fwdLong[d] );
+				Assert.assertEquals( bckVal, bckLong[d] );
+				
+				Assert.assertEquals( fwdVal, raFwd.getIntPosition( d ) );
+				Assert.assertEquals( bckVal, raBck.getIntPosition( d ) );
+				
+			}
+			
+		}
+		
+	}
+
+}
