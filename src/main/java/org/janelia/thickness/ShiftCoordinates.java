aedfa7a6e861011592afc2dd1002b0f552b88fee
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2015-02-03 18:02:27 -0500

    comment out log

--- src/main/java/org/janelia/thickness/ShiftCoordinates.java
+++ src/main/java/org/janelia/thickness/ShiftCoordinates.java
@@ -1,7 +1,5 @@
 package org.janelia.thickness;
 
-import ij.IJ;
-
 import java.util.ArrayList;
 import java.util.TreeMap;
 
@@ -66,9 +64,9 @@ public class ShiftCoordinates {
 				
 //				localShifts.add( new ConstantPair<Double, Double>( shift, weights[ i ] * 1.0 / ( Math.abs( i - k ) + 1 ) ) );
 				localShifts.add( new ConstantPair<Double, Double>( shift, weights[ i ] ) );
-				if ( k == 350 ) {
-					IJ.log( k +  " from " + i + ": s=" + shift + ", m=" + m +  ", r=" + corrAccess.get().get() );
-				}
+//				if ( k == 350 ) {
+//					IJ.log( k +  " from " + i + ": s=" + shift + ", m=" + m +  ", r=" + corrAccess.get().get() );
+//				}
 			}
 		}
 		return weightedShifts;
