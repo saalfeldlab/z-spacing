8445f5cdcd9f3b58837bb481f189dc017e079b49
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2016-07-22 10:57:46 -0400

    ZPositionCorrection plugin now accetps matrix AND strip

--- src/main/java/org/janelia/thickness/plugin/ZPositionCorrection.java
+++ src/main/java/org/janelia/thickness/plugin/ZPositionCorrection.java
@@ -15,6 +15,7 @@ import org.janelia.thickness.lut.LUTRealTransform;
 import org.janelia.thickness.lut.PermutationTransform;
 import org.janelia.thickness.lut.SingleDimensionLUTRealTransform;
 import org.janelia.thickness.lut.SingleDimensionPermutationTransform;
+import org.janelia.utility.MatrixStripConversion;
 import org.janelia.utility.arrays.ArraySortedIndices;
 
 import fiji.util.gui.GenericDialogPlus;
@@ -59,10 +60,10 @@ public class ZPositionCorrection implements PlugIn
 {
 
 	@Override
-	public void run( String arg0 )
+	public void run( final String arg0 )
 	{
 
-		Options options = Options.generateDefaultOptions();
+		final Options options = Options.generateDefaultOptions();
 
 		final GenericDialogPlus dialog = new GenericDialogPlus( "Correct layer z-positions" );
 		dialog.addMessage( "Data source settings : " );
@@ -83,9 +84,9 @@ public class ZPositionCorrection implements PlugIn
 		if ( dialog.wasCanceled() )
 			return;
 
-		String inputPath = dialog.getNextString();
-		boolean inputIsMatrix = dialog.getNextChoiceIndex() == 0;
-		ImagePlus input = inputPath.equals( "" ) ? IJ.getImage() : FolderOpener.open( inputPath );
+		final String inputPath = dialog.getNextString();
+		final boolean inputIsMatrix = dialog.getNextChoiceIndex() == 0;
+		final ImagePlus input = inputPath.equals( "" ) ? IJ.getImage() : FolderOpener.open( inputPath );
 
 		options.comparisonRange = ( int ) dialog.getNextNumber();
 		options.nIterations = ( int ) dialog.getNextNumber();
@@ -97,154 +98,159 @@ public class ZPositionCorrection implements PlugIn
 		options.minimumSectionThickness = 1e-9;
 		options.regularizationType = InferFromMatrix.RegularizationType.BORDER;
 
-		FloatProcessor matrixFp = inputIsMatrix ? normalize( input ).getProcessor().convertToFloatProcessor() : calculateSimilarityMatrix( input, options.comparisonRange );
+		final FloatProcessor matrixFp = inputIsMatrix ? normalize( input ).getProcessor().convertToFloatProcessor() : calculateSimilarityMatrix( input, options.comparisonRange );
 
 		if ( matrixFp == null )
 			return;
 
-		RandomAccessibleInterval< DoubleType > matrix = wrapDouble( new ImagePlus( "", matrixFp ) );
+		final boolean isStrip = matrixFp.getWidth() != matrixFp.getHeight();
+		final RandomAccessibleInterval< DoubleType > wrappedFp = wrapDouble( new ImagePlus( "", matrixFp ) );
 
-		if ( !inputIsMatrix )
-			ImageJFunctions.show( matrix );
+		final RandomAccessibleInterval< DoubleType > matrix = isStrip ?
+				MatrixStripConversion.stripToMatrix( wrappedFp, new DoubleType() ) : wrappedFp;
 
-		double[] startingCoordinates = new double[ ( int ) matrix.dimension( 0 ) ];
-		for ( int i = 0; i < startingCoordinates.length; i++ )
-			startingCoordinates[ i ] = i;
+				if ( !inputIsMatrix )
+					ImageJFunctions.show( matrix );
 
-		InferFromMatrix inf = new InferFromMatrix( new GlobalCorrelationFitAverage() );
+				final double[] startingCoordinates = new double[ ( int ) matrix.dimension( 0 ) ];
+				for ( int i = 0; i < startingCoordinates.length; i++ )
+					startingCoordinates[ i ] = i;
 
-		boolean estimatedSuccessfully = false;
-		double[] transform = null;
-		try
-		{
-			transform = inf.estimateZCoordinates( matrix, startingCoordinates, new LazyVisitor(), options );
-			estimatedSuccessfully = true;
-		}
-		catch ( NotEnoughDataPointsException e )
-		{
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		catch ( IllDefinedDataPointsException e )
-		{
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		catch ( Exception e )
-		{
-			e.printStackTrace();
-		}
-		System.out.println( options.toString() );
+				final InferFromMatrix inf = new InferFromMatrix( new GlobalCorrelationFitAverage() );
 
-		if ( estimatedSuccessfully )
-		{
-
-			IJ.log( Arrays.toString( transform ) );
-			boolean renderTransformedStack = false;
-
-			double[] sortedTransform = transform.clone();
-			int[] forward = new int[ sortedTransform.length ];
-			int[] backward = new int[ sortedTransform.length ];
-
-			if ( options.withReorder )
-				ArraySortedIndices.sort( sortedTransform, forward, backward );
-			else
-			{
-				for ( int i = 0; i < forward.length; ++i )
+				boolean estimatedSuccessfully = false;
+				double[] transform = null;
+				try
 				{
-					forward[ i ] = i;
-					backward[ i ] = i;
+					transform = inf.estimateZCoordinates( matrix, startingCoordinates, new LazyVisitor(), options );
+					estimatedSuccessfully = true;
 				}
-			}
+				catch ( final NotEnoughDataPointsException e )
+				{
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				catch ( final IllDefinedDataPointsException e )
+				{
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				catch ( final Exception e )
+				{
+					e.printStackTrace();
+				}
+				System.out.println( options.toString() );
 
-			int[] permutationArray = backward; // use backward?
+				if ( estimatedSuccessfully )
+				{
 
-			PermutationTransform permutation = new PermutationTransform( permutationArray, 2, 2 );
-			LUTRealTransform lut = new LUTRealTransform( sortedTransform, 2, 2 );
-			RealRandomAccessible< DoubleType > transformedMatrix = generateTransformed( matrix, permutation, lut, new DoubleType( Double.NaN ) );
+					IJ.log( Arrays.toString( transform ) );
+					boolean renderTransformedStack = false;
 
-			ImageJFunctions.show( Views.interval( Views.raster( transformedMatrix ), matrix ), "Warped matrix" );
+					final double[] sortedTransform = transform.clone();
+					final int[] forward = new int[ sortedTransform.length ];
+					final int[] backward = new int[ sortedTransform.length ];
 
-			double renderingZScale = 1.0;
-			final GenericDialogPlus renderDialog = new GenericDialogPlus( "Rendering." );
-			renderDialog.addCheckbox( "Render stack?", renderTransformedStack );
-			if ( inputIsMatrix )
-				renderDialog.addFileField( "Input path (use current image if empty)", "" );
-			renderDialog.addNumericField( "Scale result stack", renderingZScale, 4 );
-			renderDialog.showDialog();
+					if ( options.withReorder )
+						ArraySortedIndices.sort( sortedTransform, forward, backward );
+					else
+						for ( int i = 0; i < forward.length; ++i )
+						{
+							forward[ i ] = i;
+							backward[ i ] = i;
+						}
 
-			if ( renderDialog.wasCanceled() )
-				return;
+					final int[] permutationArray = backward; // use backward?
 
-			renderTransformedStack = renderDialog.getNextBoolean();
-			renderingZScale = renderDialog.getNextNumber();
+					final PermutationTransform permutation = new PermutationTransform( permutationArray, 2, 2 );
+					final LUTRealTransform lut = new LUTRealTransform( sortedTransform, 2, 2 );
+					final IntervalView< DoubleType > transformedStripOrMatrix = Views.interval( Views.raster(
+							generateTransformed( matrix, permutation, lut, new DoubleType( Double.NaN ) ) ), matrix );
+					final RandomAccessibleInterval< DoubleType > transformedMatrix = isStrip ? MatrixStripConversion.matrixToStrip( transformedStripOrMatrix, options.comparisonRange, new DoubleType() ) : transformedStripOrMatrix;
 
-			if ( renderTransformedStack )
-			{
-				ImagePlus stackImp = inputIsMatrix ? getFileFromOption( renderDialog.getNextString() ) : input;
-				new ImageConverter( stackImp ).convertToGray32();
-				RandomAccessibleInterval< FloatType > stack = ImageJFunctions.wrapFloat( stackImp );
 
-				SingleDimensionPermutationTransform permutation1D = new SingleDimensionPermutationTransform( permutationArray, 3, 3, 2 );
-				SingleDimensionLUTRealTransform lut1D = new SingleDimensionLUTRealTransform( sortedTransform, 3, 3, 2 );
+					ImageJFunctions.show( transformedMatrix, "Warped matrix" );
 
-				ImageStack transformedStack =
-						generateStack(
-								generateTransformed( stack, permutation1D, lut1D, new FloatType( Float.NaN ) ),
-								stackImp.getWidth(),
-								stackImp.getHeight(),
-								stackImp.getStackSize(),
-								renderingZScale );
+					double renderingZScale = 1.0;
+					final GenericDialogPlus renderDialog = new GenericDialogPlus( "Rendering." );
+					renderDialog.addCheckbox( "Render stack?", renderTransformedStack );
+					if ( inputIsMatrix )
+						renderDialog.addFileField( "Input path (use current image if empty)", "" );
+					renderDialog.addNumericField( "Scale result stack", renderingZScale, 4 );
+					renderDialog.showDialog();
 
-				ImagePlus resultImp = new ImagePlus( "Warped image stack", transformedStack );
-				resultImp.show();
+					if ( renderDialog.wasCanceled() )
+						return;
 
-				IJ.log( "Rendered warped image stack." );
-			}
+					renderTransformedStack = renderDialog.getNextBoolean();
+					renderingZScale = renderDialog.getNextNumber();
 
-		}
+					if ( renderTransformedStack )
+					{
+						final ImagePlus stackImp = inputIsMatrix ? getFileFromOption( renderDialog.getNextString() ) : input;
+						new ImageConverter( stackImp ).convertToGray32();
+						final RandomAccessibleInterval< FloatType > stack = ImageJFunctions.wrapFloat( stackImp );
+
+						final SingleDimensionPermutationTransform permutation1D = new SingleDimensionPermutationTransform( permutationArray, 3, 3, 2 );
+						final SingleDimensionLUTRealTransform lut1D = new SingleDimensionLUTRealTransform( sortedTransform, 3, 3, 2 );
+
+						final ImageStack transformedStack =
+								generateStack(
+										generateTransformed( stack, permutation1D, lut1D, new FloatType( Float.NaN ) ),
+										stackImp.getWidth(),
+										stackImp.getHeight(),
+										stackImp.getStackSize(),
+										renderingZScale );
+
+						final ImagePlus resultImp = new ImagePlus( "Warped image stack", transformedStack );
+						resultImp.show();
+
+						IJ.log( "Rendered warped image stack." );
+					}
+
+				}
 
 	}
 
-	public static RandomAccessibleInterval< DoubleType > wrapDouble( ImagePlus input )
+	public static RandomAccessibleInterval< DoubleType > wrapDouble( final ImagePlus input )
 	{
 		return new ConvertedRandomAccessibleInterval< FloatType, DoubleType >( ImageJFunctions.wrapFloat( input ), new RealDoubleConverter< FloatType >(), new DoubleType() );
 	}
 
-	public static ImagePlus normalize( ImagePlus input )
+	public static ImagePlus normalize( final ImagePlus input )
 	{
-		FloatProcessor fp = input.getProcessor().convertToFloatProcessor();
-		FloatStatistics stat = new FloatStatistics( fp );
-		float[] array = ( float[] ) fp.getPixels();
+		final FloatProcessor fp = input.getProcessor().convertToFloatProcessor();
+		final FloatStatistics stat = new FloatStatistics( fp );
+		final float[] array = ( float[] ) fp.getPixels();
 		for ( int i = 0; i < array.length; ++i )
 			array[ i ] /= stat.max;
 		return input;
 	}
 
-	public static RandomAccessibleInterval< DoubleType > normalizeAndWrap( ImagePlus input )
+	public static RandomAccessibleInterval< DoubleType > normalizeAndWrap( final ImagePlus input )
 	{
 		return wrapDouble( normalize( input ) );
 	}
 
-	public static FloatProcessor calculateSimilarityMatrix( ImagePlus input, int range )
+	public static FloatProcessor calculateSimilarityMatrix( final ImagePlus input, final int range )
 	{
-		GenericDialog dialog = new GenericDialog( "Choose similiarity calculation method" );
+		final GenericDialog dialog = new GenericDialog( "Choose similiarity calculation method" );
 		dialog.addChoice( "Similarity_method :", new String[] { "NCC (aligned)" }, "NCC (aligned)" );
 		dialog.showDialog();
 
 		if ( dialog.wasCanceled() )
 			return null;
 
-		int method = dialog.getNextChoiceIndex();
-		FloatProcessor matrix = createEmptyMatrix( input.getStack().getSize() );
+		final int method = dialog.getNextChoiceIndex();
+		final FloatProcessor matrix = createEmptyMatrix( input.getStack().getSize() );
 
 		boolean similarityCalculationWasSuccessful = false;
 		switch ( method )
 		{
 		case 1:
 			similarityCalculationWasSuccessful = invokeSIFT( input, range, matrix ); // not
-																						// implemented
-																						// yet
+			// implemented
+			// yet
 		default:
 			similarityCalculationWasSuccessful = invokeNCC( input, range, matrix );
 		}
@@ -254,39 +260,40 @@ public class ZPositionCorrection implements PlugIn
 			return null;
 	}
 
-	public static void main( String[] args )
+	public static void main( final String[] args )
 	{
 		new ImageJ();
 //		ImagePlus imp = new ImagePlus("/data/hanslovskyp/davi_toy_set/substacks/shuffle/03/data/data.tif");
 //		ImagePlus imp = new FolderOpener().openFolder( "/data/hanslovskyp/forPhilipp/substacks/03/data/" );
-		ImagePlus imp = new FolderOpener().openFolder( "/data/hanslovskyp/davi_toy_set/data/seq" );
+//		ImagePlus imp = new FolderOpener().openFolder( "/data/hanslovskyp/davi_toy_set/data/seq" );
+		final ImagePlus imp = new ImagePlus( "/data/hanslovskyp/strip-example-small.tif" );
 		imp.show();
 		new ZPositionCorrection().run( "" );
 	}
 
-	public static boolean invokeSIFT( ImagePlus input, int range, FloatProcessor matrix )
+	public static boolean invokeSIFT( final ImagePlus input, final int range, final FloatProcessor matrix )
 	{
 		// TODO IMPLEMENT
 		return false;
 	}
 
-	public static boolean invokeNCC( ImagePlus input, final int range, final FloatProcessor matrix )
+	public static boolean invokeNCC( final ImagePlus input, final int range, final FloatProcessor matrix )
 	{
 		new ImageConverter( input ).convertToGray32();
-		ImageStack stackSource = input.getStack();
+		final ImageStack stackSource = input.getStack();
 
-		GenericDialog dialog = new GenericDialog( "NCC options" );
+		final GenericDialog dialog = new GenericDialog( "NCC options" );
 		dialog.addNumericField( "Scale xy before similarity calculation", 1.0, 3 );
 		dialog.showDialog();
 		if ( dialog.wasCanceled() )
 			return false;
 
-		double xyScale = dialog.getNextNumber();
+		final double xyScale = dialog.getNextNumber();
 
 		final ImageStack stack = xyScale == 1.0 ? stackSource : downsampleStack( stackSource, xyScale );
 		final int height = input.getStackSize();
 		final int nThreads = Runtime.getRuntime().availableProcessors();
-		ArrayList< Callable< Void > > callables = new ArrayList< Callable< Void > >();
+		final ArrayList< Callable< Void > > callables = new ArrayList< Callable< Void > >();
 		for ( int i = 0; i < height; ++i )
 		{
 			final int finalI = i;
@@ -298,7 +305,7 @@ public class ZPositionCorrection implements PlugIn
 				{
 					for ( int k = finalI + 1; k - finalI <= range && k < height; ++k )
 					{
-						float val = new RealSumFloatNCC( ( float[] ) stack.getProcessor( finalI + 1 ).getPixels(), ( float[] ) stack.getProcessor( k + 1 ).getPixels() ).call().floatValue();
+						final float val = new RealSumFloatNCC( ( float[] ) stack.getProcessor( finalI + 1 ).getPixels(), ( float[] ) stack.getProcessor( k + 1 ).getPixels() ).call().floatValue();
 						matrix.setf( finalI, k, val );
 						matrix.setf( k, finalI, val );
 					}
@@ -306,12 +313,12 @@ public class ZPositionCorrection implements PlugIn
 				}
 			} );
 		}
-		ExecutorService es = Executors.newFixedThreadPool( nThreads );
+		final ExecutorService es = Executors.newFixedThreadPool( nThreads );
 		try
 		{
 			es.invokeAll( callables );
 		}
-		catch ( InterruptedException e )
+		catch ( final InterruptedException e )
 		{
 			e.printStackTrace();
 			return false;
@@ -319,69 +326,67 @@ public class ZPositionCorrection implements PlugIn
 		return true;
 	}
 
-	public static ImageStack downsampleStack( ImageStack stackSource, double xyScale )
+	public static ImageStack downsampleStack( final ImageStack stackSource, final double xyScale )
 	{
-		ImageStack stack = new ImageStack( ( int ) Math.round( stackSource.getWidth() * xyScale ), ( int ) Math.round( stackSource.getHeight() * xyScale ) );
+		final ImageStack stack = new ImageStack( ( int ) Math.round( stackSource.getWidth() * xyScale ), ( int ) Math.round( stackSource.getHeight() * xyScale ) );
 		for ( int z = 1; z <= stackSource.getSize(); ++z )
-		{
 			stack.addSlice( Filter.createDownsampled(
 					stackSource.getProcessor( z ),
 					xyScale,
 					0.5f,
 					0.5f ) );
-		}
 		return stack;
 	}
 
-	public static FloatProcessor createEmptyMatrix( int height )
+	public static FloatProcessor createEmptyMatrix( final int height )
 	{
-		FloatProcessor matrix = new FloatProcessor( height, height );
+		final FloatProcessor matrix = new FloatProcessor( height, height );
 		matrix.add( Double.NaN );
 		for ( int i = 0; i < height; ++i )
 			matrix.setf( i, i, 1.0f );
 		return matrix;
 	}
 
-	public static ImagePlus getFileFromOption( String path )
+	public static ImagePlus getFileFromOption( final String path )
 	{
 		return path.equals( "" ) ? IJ.getImage() : ( new File( path ).isDirectory() ? FolderOpener.open( path ) : new ImagePlus( path ) );
 	}
 
 	public static < T extends RealType< T > > RealRandomAccessible< T > generateTransformed(
-			RandomAccessibleInterval< T > input,
-			Transform permutation,
-			InvertibleRealTransform lut,
-			T dummy )
+			final RandomAccessibleInterval< T > input,
+			final Transform permutation,
+			final InvertibleRealTransform lut,
+			final T dummy )
 	{
 		dummy.setReal( Double.NaN );
-		IntervalView< T > permuted = Views.interval( new TransformView< T >( input, permutation ), input );
-		RealRandomAccessible< T > interpolated = Views.interpolate( Views.extendValue( permuted, dummy ), new NLinearInterpolatorFactory< T >() );
+		final IntervalView< T > permuted = Views.interval( new TransformView< T >( input, permutation ), input );
+		final RealRandomAccessible< T > interpolated = Views.interpolate( Views.extendValue( permuted, dummy ), new NLinearInterpolatorFactory< T >() );
 		return RealViews.transformReal( interpolated, lut );
 	}
 
 	public static < T extends RealType< T > > ImageStack generateStack(
-			RealRandomAccessible< T > input,
+			final RealRandomAccessible< T > input,
 			final int width,
 			final int height,
 			final int size,
 			final double zScale )
 	{
-		Scale3D scaleTransform = new Scale3D( 1.0, 1.0, zScale );
-		RealTransformRealRandomAccessible< T, InverseRealTransform > scaledInput = RealViews.transformReal( input, scaleTransform );
-		int scaledSize = ( int ) ( size * zScale );
+		final Scale3D scaleTransform = new Scale3D( 1.0, 1.0, zScale );
+		final RealTransformRealRandomAccessible< T, InverseRealTransform > scaledInput = RealViews.transformReal( input, scaleTransform );
+		final int scaledSize = ( int ) ( size * zScale );
 		return generateStack( scaledInput, width, height, scaledSize );
 	}
 
 	public static < T extends RealType< T > > ImageStack generateStack(
-			RealRandomAccessible< T > input,
+			final RealRandomAccessible< T > input,
 			final int width,
 			final int height,
 			final int size )
 	{
 		final ImageStack stack = new ImageStack( width, height, size );
-		int nThreads = Runtime.getRuntime().availableProcessors();
-		ExecutorService es = Executors.newFixedThreadPool( nThreads );
-		ArrayList< Callable< Void > > callables = new ArrayList< Callable< Void > >();
+		final int nThreads = Runtime.getRuntime().availableProcessors();
+		final ExecutorService es = Executors.newFixedThreadPool( nThreads );
+		final ArrayList< Callable< Void > > callables = new ArrayList< Callable< Void > >();
 		for ( int z = 0; z < size; ++z )
 		{
 			final int zeroBased = z;
@@ -416,7 +421,7 @@ public class ZPositionCorrection implements PlugIn
 		{
 			es.invokeAll( callables );
 		}
-		catch ( InterruptedException e )
+		catch ( final InterruptedException e )
 		{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
