bd69deb6c060e7ce6516ccd6562b28ece4164dd9
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2016-07-27 18:03:33 -0400

    applied imglib2 formatter to MatrixStripConversion

--- src/main/java/org/janelia/utility/MatrixStripConversion.java
+++ src/main/java/org/janelia/utility/MatrixStripConversion.java
@@ -11,32 +11,32 @@ import net.imglib2.view.TransformView;
 import net.imglib2.view.Views;
 
 /**
- * 
+ *
  * @author Philipp Hanslovsky &lt;hanslovskyp@janelia.hhmi.org&gt;
  *
  */
 public class MatrixStripConversion
 {
 	public static < T extends Type< T > > RandomAccessibleInterval< T >
-			stripToMatrix( RandomAccessibleInterval< T > strip, T dummy )
+	stripToMatrix( final RandomAccessibleInterval< T > strip, final T dummy )
 	{
-		ExtendedRandomAccessibleInterval< T, RandomAccessibleInterval< T > > extended = Views.extendValue( strip, dummy );
-		AbstractShearTransform tf = new ShearTransform( 2, 0, 1 ).inverse();
+		final ExtendedRandomAccessibleInterval< T, RandomAccessibleInterval< T > > extended = Views.extendValue( strip, dummy );
+		final AbstractShearTransform tf = new ShearTransform( 2, 0, 1 ).inverse();
 		final long w = strip.dimension( 0 ) / 2;
 		final long h = strip.dimension( 1 );
-		FinalInterval interval = new FinalInterval( new long[] { w, 0 }, new long[] { h + w - 1, h - 1 } );
-		IntervalView< T > transformed = Views.offsetInterval( new TransformView<>( extended, tf ), interval );
+		final FinalInterval interval = new FinalInterval( new long[] { w, 0 }, new long[] { h + w - 1, h - 1 } );
+		final IntervalView< T > transformed = Views.offsetInterval( new TransformView<>( extended, tf ), interval );
 		return transformed;
 	}
 
 	public static < T extends Type< T > > RandomAccessibleInterval< T >
-			matrixToStrip( RandomAccessibleInterval< T > matrix, int range, T dummy )
+	matrixToStrip( final RandomAccessibleInterval< T > matrix, final int range, final T dummy )
 	{
-		ExtendedRandomAccessibleInterval< T, RandomAccessibleInterval< T > > extended = Views.extendValue( matrix, dummy );
-		AbstractShearTransform tf = new ShearTransform( 2, 0, 1 );
+		final ExtendedRandomAccessibleInterval< T, RandomAccessibleInterval< T > > extended = Views.extendValue( matrix, dummy );
+		final AbstractShearTransform tf = new ShearTransform( 2, 0, 1 );
 		final long h = matrix.dimension( 1 );
-		FinalInterval interval = new FinalInterval( new long[] { -range, 0 }, new long[] { range, h } );
-		IntervalView< T > transformed = Views.offsetInterval( new TransformView<>( extended, tf ), interval );
+		final FinalInterval interval = new FinalInterval( new long[] { -range, 0 }, new long[] { range, h } );
+		final IntervalView< T > transformed = Views.offsetInterval( new TransformView<>( extended, tf ), interval );
 		return transformed;
 	}
 }
