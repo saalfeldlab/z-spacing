e594dff35261aa62598b433fb3c0d47a0bd480f6
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2018-04-03 14:13:23 -0400

    Add convenience methods for RealType

--- src/main/java/org/janelia/utility/MatrixStripConversion.java
+++ src/main/java/org/janelia/utility/MatrixStripConversion.java
@@ -5,6 +5,8 @@ import net.imglib2.RandomAccessibleInterval;
 import net.imglib2.transform.integer.shear.AbstractShearTransform;
 import net.imglib2.transform.integer.shear.ShearTransform;
 import net.imglib2.type.Type;
+import net.imglib2.type.numeric.RealType;
+import net.imglib2.util.Util;
 import net.imglib2.view.ExtendedRandomAccessibleInterval;
 import net.imglib2.view.IntervalView;
 import net.imglib2.view.TransformView;
@@ -17,10 +19,20 @@ import net.imglib2.view.Views;
  */
 public class MatrixStripConversion
 {
-	public static < T extends Type< T > > RandomAccessibleInterval< T >
-	stripToMatrix( final RandomAccessibleInterval< T > strip, final T dummy )
+
+	public static < T extends RealType< T > > RandomAccessibleInterval< T > stripToMatrix(
+			final RandomAccessibleInterval< T > strip )
 	{
-		final ExtendedRandomAccessibleInterval< T, RandomAccessibleInterval< T > > extended = Views.extendValue( strip, dummy );
+		T extension = Util.getTypeFromInterval( strip ).createVariable();
+		extension.setReal( Double.NaN );
+		return stripToMatrix( strip, extension );
+	}
+
+	public static < T extends Type< T > > RandomAccessibleInterval< T > stripToMatrix(
+			final RandomAccessibleInterval< T > strip,
+			final T extension )
+	{
+		final ExtendedRandomAccessibleInterval< T, RandomAccessibleInterval< T > > extended = Views.extendValue( strip, extension );
 //		final ExtendedRandomAccessibleInterval< T, RandomAccessibleInterval< T > > extended = Views.extendValue( Views.expandBorder( strip, 1, 1 ), dummy );
 		final AbstractShearTransform tf = new ShearTransform( 2, 0, 1 ).inverse();
 		final long w = strip.dimension( 0 ) / 2;
@@ -30,10 +42,21 @@ public class MatrixStripConversion
 		return transformed;
 	}
 
-	public static < T extends Type< T > > RandomAccessibleInterval< T >
-	matrixToStrip( final RandomAccessibleInterval< T > matrix, final int range, final T dummy )
+	public static < T extends RealType< T > > RandomAccessibleInterval< T > matrixToStrip(
+			final RandomAccessibleInterval< T > matrix,
+			final int range )
 	{
-		final ExtendedRandomAccessibleInterval< T, RandomAccessibleInterval< T > > extended = Views.extendValue( matrix, dummy );
+		T extension = Util.getTypeFromInterval( matrix ).createVariable();
+		extension.setReal( Double.NaN );
+		return matrixToStrip( matrix, range, extension );
+	}
+
+	public static < T extends Type< T > > RandomAccessibleInterval< T > matrixToStrip(
+			final RandomAccessibleInterval< T > matrix,
+			final int range,
+			final T extension )
+	{
+		final ExtendedRandomAccessibleInterval< T, RandomAccessibleInterval< T > > extended = Views.extendValue( matrix, extension );
 		final AbstractShearTransform tf = new ShearTransform( 2, 0, 1 );
 		final long h = matrix.dimension( 1 );
 		final FinalInterval interval = new FinalInterval( new long[] { -range, 0 }, new long[] { range, h - 1 } );
